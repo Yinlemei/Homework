a66a3fec83718e5370e671050042f82f367b671f
zmhab <zmhab31@yeah.net>
2018-02-02 18:53:40 +0800

    Revert "第二次提交的作业"
    This reverts commit ecfabc0f5cb851718c55898c3b27b4bcd275952d.

--- Homework-How-to-use-Github/李泽旭/第二章作业13.（1）.py
+++
@@ -1,4 +0,0 @@
-a = 1
-for i in range(10):
-    print(a)
-    a = a + 1
--- Homework-How-to-use-Github/李泽旭/第二章作业9..py
+++
@@ -1,8 +0,0 @@
-spam = int(input())
-if spam == 1:
-    print('Hello')
-elif spam == 2:
-    print('Howdy')
-else:
-    print('Greeting!')
-    
