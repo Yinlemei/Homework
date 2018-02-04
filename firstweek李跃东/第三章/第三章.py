fd2f38038a8b09da838ddb9df5fed96d469f7d63
LYD66 <2497785119@qq.com>
2018-02-04 21:49:28 +0800

    前三章学习
    基本函数，语句的学习

---
+++ firstweek李跃东/第三章/第三章.py
@@ -0,0 +1,9 @@
+# collatz():
+number = int(input("请输入数字")
+ A=number%2
+    if A = = 0:
+        print ("number//2")
+        return number
+    elif A = = 1:
+        print(collatz())
+        return(3 * number + 1)
---
+++ firstweek李跃东/第二章/第二章9题.py
@@ -0,0 +1,7 @@
+spam=input("请输入数字")
+if spam == '1':
+    print("Hello")
+elif spam == '2':
+    print("Howdy")
+else:
+    print("Greetings")
